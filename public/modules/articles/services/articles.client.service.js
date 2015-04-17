8d2c92167045a165b2346086742b265806974c60
grooveymo <ingeniumsoft@gmail.com>
2015-04-17 14:57:40 +0100

    first commit

---
+++ public/modules/articles/services/articles.client.service.js
@@ -0,0 +1,38 @@
+'use strict';
+
+//Articles service used for communicating with the articles REST endpoints
+angular.module('articles').factory('Articles', ['$resource',
+	function($resource) {
+		return $resource('articles/:articleId', {
+			articleId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+])
+    //[mo]
+.factory('socket', function ($rootScope) {
+var socket = io.connect();
+return {
+    on: function (eventName, callback) {
+        socket.on(eventName, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                callback.apply(socket, args);
+            });
+        });
+    },
+    emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                if (callback) {
+                    callback.apply(socket, args);
+                }
+            });
+        })
+    }
+};
+});
